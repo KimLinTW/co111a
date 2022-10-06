7c39c95c6268ea423170e0b574610850592a9218
Kim Lin <linkim0914@gmail.com>
2022-10-06 11:37:38 +0800

    1006 homework(1/2)

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Set R2 to R0*R1
+
+
+//set R2 = 0 init
+@2
+M=0
+
+(LOOP)
+    // if R0<=0 goto exit1;
+    @0
+    D=M
+    @exit1
+    D;JLE
+
+    //R2 = R2 + R1
+    @1
+    D=M
+    @2
+    M=D+M
+
+    //R0 = R0 - 1
+    @0
+    D=M
+    @0
+    M=1
+    M=D-M
+
+@LOOP
+0;JMP // jump to R[A]
+
+(exit1)
+@exit1
+0;JMP
+
+
